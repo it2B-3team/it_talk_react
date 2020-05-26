00723423ffefbacf70f95fc42845a9824fdcf6c0
unknown <gptjd1997@gmail.com>
2020-05-26 18:35:10 +0900

    bulb Icon

--- src/App.js
+++ src/App.js
@@ -7,6 +7,6 @@ function App() {
       <Login />
     </div>
   );
-
+}
 
 export default App;
--- src/Login/Style.js
+++ src/Login/Style.js
@@ -3,37 +3,15 @@ import React from "react";
 
 export const Box = styled.div`
   margin: 0 auto;
-  margin-top: 30px;
-  width: 800px;
-  height: 800px;
-  background-color: ghostwhite;
-  border-radius: 20px;
+  margin-top: 15px;
+  width: 875px;
+  height: 875px;
+  background-color: #ffffff;
+  border-radius: 15px;
   padding: 30px;
-  -webkit-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
-  -moz-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
-  box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+  -webkit-box-shadow: 5px 5px 5px 0px #3ca0b4;
+  -moz-box-shadow: 5px 5px 5px 0px #3ca0b4);
+  box-shadow: 5px 5px 5px 0px #3ca0b4);
   display: flex;
   justify-content: center;
-`;
-export const Input_Box = styled.input`
-  width: 300px;
-  height: 30px;
-  margin: 0 auto;
-  border-radius: 5px;
-  padding: 0 3px;
-  &:focus {
-    -webkit-box-shadow: 0px 0px 13px -5px rgba(0, 0, 0, 0.75);
-    -moz-box-shadow: 0px 0px 13px -5px rgba(0, 0, 0, 0.75);
-    box-shadow: 0px 0px 13px -5px rgba(0, 0, 0, 0.75);
-  }
-`;
-export const Input_Form = styled.form`
-  height: 150px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-`;
-export const Login_Text = styled.h4`
-  margin: 0;
-  padding: 0 5px;
 `;
