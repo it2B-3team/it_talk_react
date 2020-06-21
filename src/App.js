4e4818f81f6ad31e9d5404b7c70fc2a376a7628f
kooeunjoo <9eunjoo9@gmail.com>
2020-06-21 20:45:13 +0900

    total design

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import Login from "./Login/Login";
 import SignUp from "./SignUp/SignUp";
 import Group from "./Group/Group";
 import GroupIn from "./GroupIn/GroupIn";
+import TotalPost from "./TotalPost/TotalPost";
+import PostIn from "./PostIn/PostIn";
+import PostUp from "./PostUp/PostUp";
 import Chat from "./Chat/Chat";
 import styled from "styled-components";
 
 function App() {
   return (
     <div className="App">
-      <GroupIn />
+      <PostUp />
     </div>
   );
 }
---
+++ src/PostIn/PostIn.js
@@ -0,0 +1,102 @@
+import React from "react";
+import {
+  Box,
+  Boxx,
+  Boxxx,
+  Boxxxx,
+  Menu,
+  TT,
+  Table,
+  Tr,
+  Td,
+  Post,
+  Back,
+  Title,
+  Comments,
+  PostMenu,
+  Plus,
+  PostBar,
+  Plan,
+} from "./Style";
+
+const PostIn = () => {
+  let posts = [
+    {
+      id: 1,
+      c: "공지사항",
+      t: "과제 제출 서둘러 주세요",
+      h: "전혜성",
+      d: "1시간 전",
+      s: "1",
+    },
+    {
+      id: 2,
+      c: "설문",
+      t: "과제 회의일 정합시당",
+      h: "구은주",
+      d: "2020.06.01",
+      s: "4",
+      content:
+        "과제 회의 언제할깡 내일, 모레 중에 언제가 좋아? 배고파 마라탕 먹자 고양이 카페 가자",
+    },
+    { id: 3, c: "", t: "", h: "", d: "", s: "" },
+    { id: 4, c: "", t: "", h: "", d: "", s: "" },
+    { id: 5, c: "", t: "", h: "", d: "", s: "" },
+    { id: 6, c: "", t: "", h: "", d: "", s: "" },
+    { id: 7, c: "", t: "", h: "", d: "", s: "" },
+    { id: 8, c: "", t: "", h: "", d: "", s: "" },
+    { id: 9, c: "", t: "", h: "", d: "", s: "" },
+  ];
+
+  let comments = [
+    { id: 1, name: "전혜성", content: "내일 어때", day: "06.01.17:00" },
+    { id: 2, name: "구은주", content: "좋앙", day: "06.01.17:30" },
+    { id: 3, name: "ㅁㄴㅇ", content: "ㅁㄴㅇㅁㄴㅇ", day: "06.01.18:00" },
+  ];
+
+  return (
+    <Box>
+      <Boxx>
+        <Menu>
+          <Back>뒤로</Back>
+          <Title>
+            <div>{posts[1].h}</div>
+            <div>{posts[1].t}</div>
+            <div>{posts[1].d}</div>
+          </Title>
+        </Menu>
+        <TT>
+          <div>
+            <Table>
+              <tbody>
+                <Tr>
+                  <Td width="600">내용</Td>
+                  <Td width="250">파일</Td>
+                </Tr>
+                <Tr>
+                  <Td width="600" height="350">
+                    {posts[1].content}
+                  </Td>
+                  <Td width="200" height="350"></Td>
+                </Tr>
+              </tbody>
+            </Table>
+            <Comments>
+              {comments.map((comment) => {
+                return (
+                  <div>
+                    <span>{comment.name}</span> <span>{comment.content}</span>
+                    <span>{comment.day}</span>
+                  </div>
+                );
+              })}
+            </Comments>
+            <Plus>+</Plus>
+          </div>
+        </TT>
+      </Boxx>
+    </Box>
+  );
+};
+
+export default PostIn;
